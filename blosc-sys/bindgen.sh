7e368b0199f5d909970aefafc1f45fb32cd296a2
Alan Somers <asomers@gmail.com>
2018-05-07 15:37:25 -0600

    Add a module-level doc comment for blosc-sys

--- blosc-sys/bindgen.sh
+++ blosc-sys/bindgen.sh
@@ -4,5 +4,5 @@ bindgen --no-rustfmt-bindings \
 	--blacklist-type __size_t \
 	--whitelist-type '.*BLOSC.*' \
 	--whitelist-function '.*blosc.*' \
-	--whitelist-var '.*BLOSC.*' /usr/local/include/blosc.h > src/lib.rs
-rustfmt --force --write-mode replace src/lib.rs
+	--whitelist-var '.*BLOSC.*' /usr/local/include/blosc.h > src/bindgen.rs
+rustfmt --force --write-mode replace src/bindgen.rs
