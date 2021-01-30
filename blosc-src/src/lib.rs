458e7058cd5b5c46f7ce55bf7c67308f7b22547e
Magnus Ulimoen <magnus@ulimoen.dev>
2021-01-30 18:57:23 +0100

    run bindgen on static bindings

--- blosc-src/src/lib.rs
+++ blosc-src/src/lib.rs
@@ -1,1 +1,6 @@
 //! This crate is a dummy for building `c-blosc`. See `blosc-sys` for how to use this
+
+#![allow(clippy::redundant_static_lifetimes)]
+#![allow(non_camel_case_types)]
+
+include!("bindgen.rs");
