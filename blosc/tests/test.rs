dd3c6531bd11d2bf83105b75dfd3c6b194444215
Alan Somers <asomers@gmail.com>
2023-03-31 11:14:25 -0600

    Replace galvanic-test with rstest
    This also necessitates raising the MSRV

--- blosc/tests/test.rs
+++ blosc/tests/test.rs
@@ -1,7 +1,8 @@
 // vim: tw=80
 
 use blosc::*;
-use galvanic_test::{fixture, test_suite};
+use rand::distributions::{Distribution, Uniform};
+use rstest::rstest;
 use std::{
     collections::hash_map::DefaultHasher,
     hash::{Hash, Hasher},
@@ -13,66 +14,46 @@ fn test_invalid_compressor() {
     assert!(Context::new().compressor(Compressor::Invalid).is_err())
 }
 
-test_suite! {
-    name round_trip;
-
-    use super::*;
-    use rand::distributions::{Distribution, Uniform};
-
-    fixture!(settings(blocksize: Option<usize>, typesize: Option<usize>,
-                      clevel: Clevel,
-                      compressor: Compressor,
-                      shuffle_mode: ShuffleMode) -> Context {
-        params {
-            vec![
-                // Baseline
-                (None, None, Clevel::L2, Compressor::LZ4, ShuffleMode::Byte),
-                // Forced blocksize
-                (Some(65536), None, Clevel::L2, Compressor::LZ4, ShuffleMode::Byte),
-                // Various compression levels
-                (None, None, Clevel::None, Compressor::LZ4, ShuffleMode::Byte),
-                (None, None, Clevel::L9, Compressor::LZ4, ShuffleMode::Byte),
-                // All different compressors
-                (None, None, Clevel::L2, Compressor::BloscLZ, ShuffleMode::Byte),
-                (None, None, Clevel::L2, Compressor::LZ4HC, ShuffleMode::Byte),
-                (None, None, Clevel::L2, Compressor::Snappy, ShuffleMode::Byte),
-                (None, None, Clevel::L2, Compressor::Zlib, ShuffleMode::Byte),
-                (None, None, Clevel::L2, Compressor::Zstd, ShuffleMode::Byte),
-                // Shuffle options
-                (None, None, Clevel::L2, Compressor::LZ4, ShuffleMode::None),
-                (None, None, Clevel::L2, Compressor::LZ4, ShuffleMode::Bit),
-                // Maximum compression
-                (None, None, Clevel::L9, Compressor::Zstd, ShuffleMode::Bit),
-                // Forced typesize
-                (None, Some(2), Clevel::L2, Compressor::LZ4, ShuffleMode::Byte),
-            ].into_iter()
-        }
-
-        setup(&mut self) {
-            Context::new()
-                .blocksize(*self.blocksize)
-                .clevel(*self.clevel)
-                .compressor(*self.compressor).unwrap()
-                .shuffle(*self.shuffle_mode)
-                .typesize(*self.typesize)
-        }
-    });
-
-    test round_trip(settings) {
-        let distribution = Uniform::new(1000u32, 2000u32);
-        let mut rng = rand::thread_rng();
-        let sample = (0..10).map(|_| {
-            distribution.sample(&mut rng)
-        }).collect::<Vec<_>>();
+#[rstest]
+#[case::baseline(None, None, Clevel::L2, Compressor::LZ4, ShuffleMode::Byte)]
+#[case::forced_blocksize(Some(65536), None, Clevel::L2, Compressor::LZ4, ShuffleMode::Byte)]
+#[case::clevel_none(None, None, Clevel::None, Compressor::LZ4, ShuffleMode::Byte)]
+#[case::clevel_9(None, None, Clevel::L9, Compressor::LZ4, ShuffleMode::Byte)]
+#[case::blosclz(None, None, Clevel::L2, Compressor::BloscLZ, ShuffleMode::Byte)]
+#[case::lz4hc(None, None, Clevel::L2, Compressor::LZ4HC, ShuffleMode::Byte)]
+#[case::snappy(None, None, Clevel::L2, Compressor::Snappy, ShuffleMode::Byte)]
+#[case::zlib(None, None, Clevel::L2, Compressor::Zlib, ShuffleMode::Byte)]
+#[case::zstd(None, None, Clevel::L2, Compressor::Zstd, ShuffleMode::Byte)]
+#[case::nosuffle(None, None, Clevel::L2, Compressor::LZ4, ShuffleMode::None)]
+#[case::bitshuffle(None, None, Clevel::L2, Compressor::LZ4, ShuffleMode::Bit)]
+#[case::maxcompress(None, None, Clevel::L9, Compressor::Zstd, ShuffleMode::Bit)]
+#[case::forced_typesize(None, Some(2), Clevel::L2, Compressor::LZ4, ShuffleMode::Byte)]
+fn round_trip(
+    #[case] blocksize: Option<usize>,
+    #[case] typesize: Option<usize>,
+    #[case] clevel: Clevel,
+    #[case] compressor: Compressor,
+    #[case] shuffle_mode: ShuffleMode
+) {
+    let ctx = Context::new()
+        .blocksize(blocksize)
+        .clevel(clevel)
+        .compressor(compressor).unwrap()
+        .shuffle(shuffle_mode)
+        .typesize(typesize);
+    let distribution = Uniform::new(1000u32, 2000u32);
+    let mut rng = rand::thread_rng();
+    let sample = (0..10).map(|_| {
+        distribution.sample(&mut rng)
+    }).collect::<Vec<_>>();
 
 
-        let encoded = settings.val.compress(&sample[..]);
-        let srclen = sample.len() * mem::size_of::<u32>();
-        let ratio = srclen as f64 / encoded.size() as f64;
-        println!("Compression ratio: {}", ratio);
-        let decoded = decompress(&encoded).unwrap();
-        assert_eq!(sample, decoded);
-    }
+    let encoded = ctx.compress(&sample[..]);
+    let srclen = sample.len() * mem::size_of::<u32>();
+    let ratio = srclen as f64 / encoded.size() as f64;
+    println!("Compression ratio: {}", ratio);
+    let decoded = decompress(&encoded).unwrap();
+    assert_eq!(sample, decoded);
 }
 
 #[test]
